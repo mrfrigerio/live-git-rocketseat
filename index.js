0f4678b1474dac3f6c897f0562d314b65774e12b
= <=>
2019-10-09 09:14:51 -0300

    chore: Cria uma constante para a porta do servidor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-
+const port = 3333
 const app = express()
 
 app.get('/teste', (req, res) => {
   return res.json({ hello: 'world!' })
 })
 
-app.listen(3333, () => console.log('Servidor rodando!'))
+app.listen(3333, () => console.log(`Servidor rodando na porta ${port}`))
