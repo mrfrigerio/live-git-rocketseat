427ba53478d5e1fbc8c21cb8c1cfdd6c907ea2a6
= <=>
2019-10-10 00:55:32 -0300

    fix: Insert the const port on app.listen() method

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ app.get('/teste', (req, res) => {
   return res.json({ hello: 'world!' })
 })
 
-app.listen(3333, () => console.log(`Servidor rodando na porta ${port}`))
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
